726ba54973a9730da2ee147d13801fe87226e6b1
Mandual <mandualba@gmail.com>
2025-04-25 21:39:55 +0200

    Update VDelete.java

--- src/mesa/view/VDelete.java
+++ src/mesa/view/VDelete.java
@@ -5,23 +5,33 @@ import java.util.Scanner;
 import animal.domain.Mesa;
 
 public class VDelete {
-	private Scanner sc ;
-    private List<Mesa> dataList;
-    public VDelete(Scanner sc, List<Mesa> l){
-        this.sc=sc;
-        dataList = l;
+    private Scanner sc;
+    private List<Mesa> mesas;
+
+    public VDelete(Scanner sc, List<Mesa> mesas) {
+        this.sc = sc;
+        this.mesas = mesas;
     }
-    public void remove(){
-        System.out.println("ELIGE EL INDICE");
-        for(int i = 0; i< dataList.size();i++){
-            System.out.println(i + " " + dataList.get(i));
+
+    public void VDelete() {
+        if (mesas.isEmpty()) {
+            System.out.println("No hay mesas disponibles para eliminar.");
+            return;
         }
+
+        System.out.println("LISTADO DE MESAS:");
+        for (int i = 0; i < mesas.size(); i++) {
+            System.out.println(i + " - " + mesas.get(i).toString());
+        }
+
+        System.out.println("INTRODUCE EL ÍNDICE DE LA MESA A ELIMINAR:");
         int posicion = sc.nextInt();
-        if(posicion >= 0 && posicion < dataList.size()){
-        	dataList.remove(posicion);
-            System.out.println("Dato eliminado");
-        }else{
-            System.out.println("POSICION INVALIDA");
+
+        if (posicion >= 0 && posicion < mesas.size()) {
+            mesas.remove(posicion);
+            System.out.println("Mesa eliminada correctamente.");
+        } else {
+            System.out.println("ÍNDICE INVÁLIDO. No se pudo eliminar.");
         }
     }
 }
