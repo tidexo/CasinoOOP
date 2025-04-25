f0880da9d0a06b70f1f903f03542fcf1a0031e75
guillermobriones <153458064+guillermobriones@users.noreply.github.com>
2025-04-25 15:08:15 +0200

    Update Poker.java

--- src/mesa/domain/Poker.java
+++ src/mesa/domain/Poker.java
@@ -1,12 +1,12 @@
 package mesa.domain;
 public class Poker extends Mesa {
-    private string modalidad;
-    public Poker(int minJugadores, int maxJugadores, boolean estadoMesa,string modalidad){
+    private String modalidad;
+    public Poker(int minJugadores, int maxJugadores, boolean estadoMesa, string modalidad){
         super(minJugadores, maxJugadores, estadoMesa);
         this.modalidad = modalidad;
     }
-    public boolean getmodalidad(){
-        return tienePelo;
+    public String getmodalidad(){
+        return modalidad;
     }   
     public String toString(){
         return "Poker, modalidad: " + modalidad + " " + super.toString();
