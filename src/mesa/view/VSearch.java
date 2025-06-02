93791a944bba39e9f6a7ae3ec5415ef1dbbfc9f9
Mandual <mandualba@gmail.com>
2025-06-02 12:13:49 +0200

    Update VSearch.java

--- src/mesa/view/VSearch.java
+++ src/mesa/view/VSearch.java
@@ -1,8 +1,8 @@
-package animal.view;
+package empleado.view;
 import java.util.List;
 import java.util.Scanner;
 
-import animal.domain.Mesa;
+import mesa.domain.Mesa;
 
 public class VSearch {
     private Scanner sc;
