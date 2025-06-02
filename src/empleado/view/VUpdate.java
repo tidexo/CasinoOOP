9746610d5d9093b0a790201f295d8bfc5ed64470
tidexo <wefwef363@gmail.com>
2025-06-02 20:28:37 +0200

    Update VUpdate.java

--- src/empleado/view/VUpdate.java
+++ src/empleado/view/VUpdate.java
@@ -1,45 +1,47 @@
-package empleado.view;
-import java.util.List;
-import java.util.Scanner;
+package CasinoOOP.src.empleado.view;
 
-import empleado.domain.Croupier;
-import empleado.domain.Empleado;
-import empleado.domain.Seguridad;
+import java.util.Scanner;
+import CasinoOOP.src.empleado.domain.Empleado;
+import CasinoOOP.src.empleado.domain.Fijo;
+import CasinoOOP.src.empleado.domain.Temporal;
+import CasinoOOP.src.common.domain.repo.IRepo;
 
 public class VUpdate {
-	private Scanner sc ;
-	private List<Empleado> dataList;
-	public VUpdate(Scanner sc, List<Empleado> l){
-		this.sc=sc;
-		dataList = l;
-	}
-	public void update(){
-		System.out.println("ELIGE EL INDICE");
-		for(int i = 0; i< dataList.size();i++){
-			System.out.println(i + " " + dataList.get(i));
-		}
-		int posicion = sc.nextInt();
-		if(posicion >= 0 && posicion < dataList.size()){
-			System.out.println("INTRODUCE NOMBRE: ");
-	        String nombre = sc.next();
-	        System.out.println("INTRODUCE SALARIO");
-	        float salario = sc.nextFloat();
-	        System.out.println("INTRODUCE CROUPIER (1) O LIMPIADOR (2) O SEGURIDAD (CUALQUIER OTRA TECLA)");
-	        int tipo = sc.nextInt();  
-	        Empleado empleado ;
-	        if(tipo == 1){
-	            System.out.println("Meses?");
-	            int meses = sc.nextInt();
-	            empleado = new Croupier(nombre, salario);
-	        }else if(tipo == 2){
-	            empleado = new Limpiador(nombre, salario, horas);
-	        }else{
-	            empleado = new Seguridad(nombre, salario, meses);
-		}
-			dataList.set(posicion,empleado);
-			System.out.println("dato actualizado");
-		}else {
-			System.out.println("POSICION INVALIDA");
-		}
-	}
+    private Scanner sc;
+    private IRepo data;
+    
+    public VUpdate(Scanner sc, IRepo data){
+        this.sc = sc;
+        this.data = data;
+    }
+    
+    public void update(){
+        System.out.println("ELIGE EL INDICE");
+        var empleados = data.getAll();
+        for(int i = 0; i < empleados.size(); i++){
+            System.out.println(i + " " + empleados.get(i));
+        }
+        int posicion = sc.nextInt();
+        if(posicion >= 0 && posicion < empleados.size()){
+            System.out.println("INTRODUCE NOMBRE: ");
+            String nombre = sc.next();
+            System.out.println("INTRODUCE SALARIO");
+            float salario = sc.nextFloat();
+            System.out.println("INTRODUCE Croupier (1) O Limpiador (2) o seguridad (otra tecla)");
+            int tipo = sc.nextInt();  
+            Empleado empleado;
+            if(tipo == 1){
+                System.out.println("Meses?");
+                int meses = sc.nextInt();
+                empleado = new Croupier(nombre, salario, meses);
+            }else if (tipo ==2){
+                empleado = new Limpiador(nombre, salario);
+            }else{
+		    empleado = new Seguridad(nombre, salario);}
+            data.update(posicion, empleado);
+            System.out.println("dato actualizado");
+        }else{
+            System.out.println("POSICION INVALIDA");
+        }
+    }
 }
