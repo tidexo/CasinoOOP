2b8a0174f2f0ce6f624159acf9247d6f038af4e8
Mandual <mandualba@gmail.com>
2025-06-02 12:12:30 +0200

    Update VCreate.java

--- src/mesa/view/VCreate.java
+++ src/mesa/view/VCreate.java
@@ -1,10 +1,10 @@
-package animal.view;
+package empleado.view;
 import java.util.List;
 import java.util.Scanner;
 
-import animal.domain.Mesa;
-import animal.domain.BlackJack;
-import animal.domain.Poker;
+import mesa.domain.Mesa;
+import blackjack.domain.BlackJack;
+import poker.domain.Poker;
 
 public class VCreate {
     private Scanner sc ;
