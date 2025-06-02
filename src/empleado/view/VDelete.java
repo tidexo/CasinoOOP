775f2930b8e6a5a290ad253816fe0b57d635940b
tidexo <wefwef363@gmail.com>
2025-06-02 20:23:55 +0200

    Update VDelete.java

--- src/empleado/view/VDelete.java
+++ src/empleado/view/VDelete.java
@@ -1,24 +1,26 @@
-package empleado.view;
-import java.util.List;
-import java.util.Scanner;
+package CasinoOOP.src.empleado.view;
 
-import empleado.domain.Empleado;
+import java.util.Scanner;
+import CasinoOOP.src.common.domain.repo.IRepo;
 
 public class VDelete {
-	private Scanner sc ;
-    private List<Empleado> dataList;
-    public VDelete(Scanner sc, List<Empleado> l){
-        this.sc=sc;
-        dataList = l;
+    private Scanner sc;
+    private IRepo data;
+    
+    public VDelete(Scanner sc, IRepo data){
+        this.sc = sc;
+        this.data = data;
     }
+    
     public void remove(){
         System.out.println("ELIGE EL INDICE");
-        for(int i = 0; i< dataList.size();i++){
-            System.out.println(i + " " + dataList.get(i));
+        var empleados = data.getAll();
+        for(int i = 0; i < empleados.size(); i++){
+            System.out.println(i + " " + empleados.get(i));
         }
         int posicion = sc.nextInt();
-        if(posicion >= 0 && posicion < dataList.size()){
-        	dataList.remove(posicion);
+        if(posicion >= 0 && posicion < empleados.size()){
+            data.delete(posicion);
             System.out.println("Dato eliminado");
         }else{
             System.out.println("POSICION INVALIDA");
